a3a4d5311e391501dd9856e2745d8f9cb51fc720
Andrés Filoso <andresfilosok@gmail.com>
2018-09-11 22:36:03 -0300

    Se cambia la estructura del proyecto. Se ordenan carpetas

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+
+Sobre este archivo se realizan configuraciones globales del proyecto. 
+Sería una buena practica poder utilizarlos.
+
+Ejemplos:
+    https://github.com/pallets/flask/blob/master/flask/config.py
+
+
+"""
