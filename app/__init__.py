ccf8d8a48bf04dc6c40d216423cbd00a02ce4014
Unknown <Gonza.cerbelli@gmail.com>
2018-09-18 10:20:23 -0300

    Cambios en scrapping

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,92 @@
-#####################
-###### init.py ######
-#####################
+#!/usr/bin/python
+from flask import Flask, render_template, request, redirect, json, session, url_for
+from app import app
+import MySQLdb
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__,
+            instance_relative_config=True,
+            template_folder='templates')
+
+
+# Ir al workbench y en "users and privileges" probar alguno que ande
+# la mayori tienden a no andar, yo la emboque no se como
+# La DB y la tabla las cree en el workbench, no se como hacerlo desde aca (todavia..)
+
+db = MySQLdb.connect(host="127.0.0.1",
+                     user="root",
+                     passwd="",
+                     db="usuario")
+
+
+
+cur = db.cursor()
+conf=False
+
+@app.route('/')
+def default():
+	return redirect(url_for('home'),code=307)
+
+
+@app.route('/home',methods = ['POST', 'GET'])
+def home():
+    return render_template('login.html')
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        result = request.form       #usar request.form porque request.get_json() no funciona
+        usr = request.form["user"]
+        passwd = str(request.form["pass"])
+
+        rows = cur.execute("SELECT * FROM usuarios where nombre = '"+usr+"' and pass = '"+passwd+"'")
+
+        if rows == 1:
+        	messages = json.dumps({"user":usr,"pass":passwd})
+        	session['messages'] = messages
+        	return redirect(url_for('lookup'),code=307)
+        else:
+        	return redirect(url_for('home'),code=307)
+    
+@app.route('/lookup',methods = ['POST', 'GET'])
+def lookup():
+
+	messages = session['messages']
+
+	return render_template('lookup.html',messages=json.loads(messages))
+
+@app.route('/scrapping',methods = ['POST', 'GET'])
+def scrapping():
+
+	arg = "data"
+
+	urlScience = "https://www.sciencedirect.com/search?qs=" + arg + "&show=10&sortBy=relevance"
+	req = requests.get(urlScience)
+	statusCode = req.status_code
+	html = BeautifulSoup(req.text, "html.parser")
+	rawScience = html.findAll('div', {'class': 'result-item-content'})
+
+	data = {}
+
+	for link in rawScience:
+	    result = "https://www.sciencedirect.com" + link.a['href']
+	    
+	    req = requests.get(result)
+	    statusCode = req.status_code
+	    html = BeautifulSoup(req.text, "html.parser")
+	    title = html.find('span', {'class': 'title-text'}).text
+	    abstract = html.find('div', {'class': 'abstract author'}).text
+
+	    print title.encode('utf8')
+	    print result.encode('utf8')
+	    print abstract.encode('utf8')
+	    
+	    data['title'] = title
+	    data['result'] = result
+	    data['abstract'] = abstract
+
+	data_ready = json.dumps(data)
+	session['data_ready'] = data_ready
+
+	return render_template('results.html',data_ready=json.loads(data_ready))
--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 #####################
 ###### run.py #######
 #####################
+from app import app
+from flask import Flask, session
+
+
+
+if __name__ == '__main__':
+
+	app.secret_key = "super secret key"
+
+	app.run()
